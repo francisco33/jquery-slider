3bd8b3fb0c81ce5a60b175283cfd4ba459d70985
brokenOne <jonatan750@gmail.com>
2015-10-29 13:47:02 +0100

    slider rebuild - classess/objects.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
 
-    // Variables
-    //––––––––––––––––––
-    var outerWidth = window.outerWidth;
-    var children;
-    var actualPosition = 0;
-    var slider = $('#main-slider');
-    var children = slider.children();
-
-    slider.children().remove();
-    slider.html('<div class="slider-wrapper-outer"></div><div class="slider-controls"></div>');
-
-    var sliderOuterWrapper = slider.find('.slider-wrapper-outer');
-    sliderOuterWrapper.html('<div class="slider-wrapper"></div>');
-
-    var sliderWrapper = sliderOuterWrapper.find('.slider-wrapper');
-    sliderWrapper.html(children);
-
-    var sliderControls = slider.find('.slider-controls');
-    var arrowLeft = '<div class="arrow arrow-left"></div>';
-    var arrowRight = '<div class="arrow arrow-right"></div>';
-    sliderControls.html(arrowLeft + arrowRight);
-
-    var sliderArrow = {
-        right : slider.find('.arrow.arrow-right'),
-        left : slider.find('.arrow.arrow-left')
-    }
-
     // Basic Functions
     //––––––––––––––––––
     function windowWidth() {
@@ -39,116 +12,19 @@ $(document).ready(function() {
         // console.log(winHeight);
         return winHeight;
     };
-    function changeSlide(elem) {
-        this.nextSlide = elem.next();
-        this.prevSlide = elem.prev();
-    }
-    function setActiveSlide(direction) {
-        activeSlide = sliderWrapper.find('.active').removeClass('active');
-        if (direction == 'next')
-            activeSlide = new changeSlide(activeSlide).nextSlide;
-        else if (direction == 'prev')
-            activeSlide = new changeSlide(activeSlide).prevSlide;
-        activeSlide.addClass('active');
-    }
-    function goToStart() {
-        actualPosition = 0;
-        sliderWrapper.css({
-            'transition': 'all 1000ms',
-            'transform': "translate3d(" + parseInt(actualPosition) + "px, 0px, 0px)",
-        });
-        sliderWrapper.find('.active').removeClass('active');
-        activeSlide = children.first().addClass('active');
-    }
-    function goToEnd() {
-        actualPosition = -(slideNumber - 1) * outerWidth;
-        sliderWrapper.css({
-            'transition': 'all 1000ms',
-            'transform': "translate3d(" + parseInt(actualPosition) + "px, 0px, 0px)",
-        });
-        sliderWrapper.find('.active').removeClass('active');
-        activeSlide = children.last().addClass('active');
-    }
-    function goToNextSlide() {
-        // Set actual childrens
-        children = sliderWrapper.children();
-
-        // Core Function
-        actualPosition = actualPosition - outerWidth;
-        sliderWrapper.css({
-            'transition': 'all 400ms',
-            'transform': "translate3d(" + parseInt(actualPosition) + "px, 0px, 0px)",
-        });
-
-        if ( activeSlide.index() + 1 == children.length ) {
-            // Go To Start
-            goToStart();
-        } else {
-            // Change active slide
-            setActiveSlide('next');
-        }
-    }
-    function goToPrevSlide() {
-        // Set actual childrens
-        children = sliderWrapper.children();
-
-        actualPosition = actualPosition + outerWidth;
-        sliderWrapper.css({
-            'transition': 'all 400ms',
-            'transform': "translate3d(" + parseInt(actualPosition) + "px, 0px, 0px)",
-        })
-
-        if ( activeSlide.index() == 0 ) {
-            goToEnd();
-        } else {
-            // Change active slide
-            setActiveSlide('prev');
-        }
-    }
 
     // Slider Init
     //––––––––––––––––––––
     $('body').css('height', windowHeight());
 
-    var slideNumber = children.length;
-    var activeSlide = children.first().addClass('active');
-
-    children.addClass('slider-item').css('width', outerWidth);
-    children = sliderWrapper.children();
-
-    sliderWrapper.css({
-        'width': children.length * outerWidth,
-        'transform': "translate3d(0px, 0px, 0px)",
-    })
-
-    // Events
-    //––––––––––––––––––
-
-    // Slider Right
-    sliderArrow.right.click(function() {
-        goToNextSlide();
+    // var slider = new Slider();
+    $('#main-slider').Slider({
+        pause: 1500,
     });
-    // Slider Left
-    sliderArrow.left.click(function() {
-        goToPrevSlide();
-    })
 
     // Functions onResize
     //––––––––––––––––––––
     $(window).resize(function() {
-
-        outerWidth = window.outerWidth;
-        actualPosition = ((-activeSlide.index()) * outerWidth);
-
-        sliderWrapper.css({
-            'width': children.length * outerWidth,
-            'transform': "translate3d(" + actualPosition + "px, 0px, 0px)",
-            'transition': 'all 0ms'
-        });
-        children.css({
-            'width': outerWidth,
-            'transition': 'all 0ms'
-        });
         $('body').css('height', windowHeight());
     });
 })
---
+++ assets/js/slider.js
@@ -0,0 +1,158 @@
+$.fn.Slider = function(options) {
+    var defaults = {
+        pause: 400,
+    };
+    var settings = $.extend({}, defaults, options);
+    var Slider = function(settings) {
+        this.o = this;
+        this.pause = options.pause;
+        this.slider = $('#main-slider');
+        this.children = this.slider.children();
+        this.activeSlide = this.children.first().addClass('active');
+        this.slideNumber = this.children.length;
+        this.sliderOuterWrapper;
+        this.sliderWrapper;
+        this.sliderArrow;
+        this.actualPosition;
+
+        function changeSlide(elem) {
+            this.nextSlide = elem.next();
+            this.prevSlide = elem.prev();
+        };
+
+        this.init = function() {
+
+            // Variables
+            //––––––––––––––––––
+            var outerWidth = window.outerWidth;
+            this.actualPosition = 0;
+
+            this.slider.children().remove();
+            this.slider.html('<div class="slider-wrapper-outer"></div><div class="slider-controls"></div>');
+
+            this.sliderOuterWrapper = this.slider.find('.slider-wrapper-outer');
+            this.sliderOuterWrapper.html('<div class="slider-wrapper"></div>');
+
+            this.sliderWrapper = this.sliderOuterWrapper.find('.slider-wrapper');
+            this.sliderWrapper.html(this.children);
+
+            var sliderControls = this.slider.find('.slider-controls');
+            var arrowLeft = '<div class="arrow arrow-left"></div>';
+            var arrowRight = '<div class="arrow arrow-right"></div>';
+
+            sliderControls.html(arrowLeft + arrowRight);
+
+            this.children.addClass('slider-item').css('width', outerWidth);
+            this.children = this.sliderWrapper.children();
+            this.sliderArrow = {
+                right : this.slider.find('.arrow.arrow-right'),
+                left : this.slider.find('.arrow.arrow-left')
+            };
+            this.sliderWrapper.css({
+                'width': this.slideNumber * outerWidth,
+                'transform': "translate3d(0px, 0px, 0px)",
+            });
+            events(this);
+        };
+        var events = function(el) {
+
+            var ob = el.o;
+
+            // Slider Right
+            el.sliderArrow.right.click(function() {
+                ob.goToNextSlide();
+            });
+            // Slider Left
+            el.sliderArrow.left.click(function() {
+                ob.goToPrevSlide();
+            });
+            // Resize
+            $(window).resize(function() {
+                ob.resize();
+            });
+        };
+        var setActiveSlide = function(el, direction) {
+            el.activeSlide = el.sliderWrapper.find('.active').removeClass('active');
+
+            if (direction == 'next')
+                el.activeSlide = new changeSlide(el.activeSlide).nextSlide;
+            else if (direction == 'prev')
+                el.activeSlide = new changeSlide(el.activeSlide).prevSlide;
+
+            el.activeSlide.addClass('active');
+        }
+        this.goToNextSlide = function() {
+            // Set actual childrens
+            this.children = this.sliderWrapper.children();
+
+            // Core Function
+            this.actualPosition = this.actualPosition - outerWidth;
+            this.sliderWrapper.css({
+                'transition': 'all ' + this.pause + 'ms',
+                'transform': "translate3d(" + parseInt(this.actualPosition) + "px, 0px, 0px)",
+            });
+
+            if ( this.activeSlide.index() + 1 == this.slideNumber ) {
+                // Go To Start
+                this.goToStart();
+            } else {
+                // Change active slide
+                setActiveSlide(this, 'next');
+            }
+        };
+        this.goToPrevSlide = function() {
+
+            // Set actual childrens
+            this.children = this.sliderWrapper.children();
+            this.actualPosition = this.actualPosition + outerWidth;
+            this.sliderWrapper.css({
+                'transition': 'all ' + this.pause + 'ms',
+                'transform': "translate3d(" + parseInt(this.actualPosition) + "px, 0px, 0px)",
+            });
+            if ( this.activeSlide.index() == 0 ) {
+                // Go To End
+                this.goToEnd();
+            } else {
+                // Change active slide
+                setActiveSlide(this, 'prev');
+            }
+        };
+        this.goToStart = function() {
+
+            this.actualPosition = 0;
+            this.sliderWrapper.css({
+                'transition': 'all 1000ms',
+                'transform': "translate3d(" + parseInt(this.actualPosition) + "px, 0px, 0px)",
+            });
+            this.sliderWrapper.find('.active').removeClass('active');
+            this.activeSlide = this.children.first().addClass('active');
+        }
+        this.goToEnd = function() {
+
+            this.actualPosition = -(this.slideNumber - 1) * outerWidth;
+            this.sliderWrapper.css({
+                'transition': 'all 1000ms',
+                'transform': "translate3d(" + parseInt(this.actualPosition) + "px, 0px, 0px)",
+            });
+            this.sliderWrapper.find('.active').removeClass('active');
+            this.activeSlide = this.children.last().addClass('active');
+        };
+        this.resize = function() {
+
+            outerWidth = window.outerWidth;
+            this.actualPosition = ((-this.activeSlide.index()) * outerWidth);
+
+            this.sliderWrapper.css({
+                'width': this.slideNumber * outerWidth,
+                'transform': "translate3d(" + this.actualPosition + "px, 0px, 0px)",
+                'transition': 'all 0ms'
+            });
+            this.children.css({
+                'width': outerWidth,
+                'transition': 'all 0ms'
+            });
+        };
+        this.init();
+    };
+    var slider = new Slider(options);
+}
